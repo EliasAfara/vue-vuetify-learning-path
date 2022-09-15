fa1ab1f2ddc81f42d57609ba8e7bb347c33a2467
Elias Afara <afaraelias@gmail.com>
2022-09-15 14:35:53 +0200

    Added template root element

--- vue-cli-demo/src/App.vue
+++ vue-cli-demo/src/App.vue
@@ -1,34 +1,36 @@
 <template>
-  <nav>
-    <header class="top-bar spread">
-      <nav class="top-bar-nav">
-        <router-link to="/" class="top-bar-link">
-          <i class="icofont-spoon-and-fork"></i>
-          <span>Home</span>
-        </router-link>
-        <router-link to="/products" class="top-bar-link">
-          <span>Products</span>
-        </router-link>
-        <router-link to="/past-orders" class="top-bar-link">
-          <span>Past Orders</span>
-        </router-link>
-      </nav>
-      <div @click="toggleSidebar" class="top-bar-cart-link">
-        <i class="icofont-cart-alt icofont-1x" />
-        <span>Cart ({{ totalItemInCart }})</span>
-      </div>
-    </header>
-  </nav>
+  <div>
+    <nav>
+      <header class="top-bar spread">
+        <nav class="top-bar-nav">
+          <router-link to="/" class="top-bar-link">
+            <i class="icofont-spoon-and-fork"></i>
+            <span>Home</span>
+          </router-link>
+          <router-link to="/products" class="top-bar-link">
+            <span>Products</span>
+          </router-link>
+          <router-link to="/past-orders" class="top-bar-link">
+            <span>Past Orders</span>
+          </router-link>
+        </nav>
+        <div @click="toggleSidebar" class="top-bar-cart-link">
+          <i class="icofont-cart-alt icofont-1x" />
+          <span>Cart ({{ totalItemInCart }})</span>
+        </div>
+      </header>
+    </nav>
 
-  <router-view :inventory="inventory" :addToCart="addToCart" />
+    <router-view :inventory="inventory" :addToCart="addToCart" />
 
-  <SidebarComponent
-    v-if="showSidebar"
-    :toggle="toggleSidebar"
-    :cart="cart"
-    :inventory="inventory"
-    :remove="removeItem"
-  />
+    <SidebarComponent
+      v-if="showSidebar"
+      :toggle="toggleSidebar"
+      :cart="cart"
+      :inventory="inventory"
+      :remove="removeItem"
+    />
+  </div>
 </template>
 
 <script>
