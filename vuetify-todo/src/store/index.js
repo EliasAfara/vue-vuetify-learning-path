9113ee7c33b8ed0444d29da668e46f88d7ce8c59
Elias Afara <afaraelias@gmail.com>
2022-09-16 11:51:14 +0200

    Setup Vuex store with init state & todo mutations

--- vuetify-todo/src/store/index.js
+++ vuetify-todo/src/store/index.js
@@ -1,17 +1,56 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    tasks: [
+      {
+        id: 1,
+        title: "Wake up",
+        done: false,
+      },
+      {
+        id: 2,
+        title: "Eat breakfast",
+        done: false,
+      },
+      {
+        id: 3,
+        title: "Go to work",
+        done: false,
+      },
+      {
+        id: 4,
+        title: "Go to sleep",
+        done: false,
+      },
+      {
+        id: 5,
+        title: "Watch TV",
+        done: false,
+      },
+    ],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    addTask(state, newTaskTitle) {
+      let newId = state.tasks.length + 1 + Math.random();
+      state.tasks.push({
+        id: newId,
+        title: newTaskTitle,
+        done: false,
+      });
+    },
+    doneTask(state, id) {
+      state.tasks.find((task) => task.id === id).done = true;
+      console.log(state.tasks);
+    },
+    deleteTask(state, id) {
+      state.tasks = state.tasks.filter((task) => task.id !== id);
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
