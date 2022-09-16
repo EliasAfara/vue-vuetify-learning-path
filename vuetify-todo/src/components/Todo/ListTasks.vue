8566f0c841709eae1334e913e046fff91a7de6ad
Elias Afara <afaraelias@gmail.com>
2022-09-16 12:09:48 +0200

    Created List tasks component

---
+++ vuetify-todo/src/components/Todo/ListTasks.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-list class="pt-0" flat>
+    <task v-for="task in $store.state.tasks" :key="task.id" :task="task" />
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: "ListTasks",
+  components: {
+    task: require("./Task.vue").default,
+  },
+};
+</script>
+
+<style>
+</style>
