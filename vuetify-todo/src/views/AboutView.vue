5641420a13382509c3c170c58dad42e25cd187ef
Elias Afara <afaraelias@gmail.com>
2022-09-16 11:48:50 +0200

    Demo about page

--- vuetify-todo/src/views/AboutView.vue
+++ vuetify-todo/src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify ToDo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam eum, ab
+      animi omnis vel dolor ipsum odit quidem praesentium obcaecati aliquid quo,
+      dolore atque voluptates natus ad cupiditate? Ipsam, facilis.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
+      exercitationem illo aliquid est repellendus, tempora ipsum optio beatae ea
+      commodi possimus, consequatur explicabo deleniti quos laboriosam nulla
+      quae blanditiis ratione.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus,
+      earum, explicabo sit ad esse non aut consequatur porro deleniti tempora
+      laudantium fuga modi veniam magni accusamus voluptatibus iure quas quidem!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit adipisci
+      ipsa nostrum nesciunt placeat corrupti corporis ad modi ab quaerat, ex,
+      ratione debitis accusantium architecto dolorem temporibus commodi, eos
+      eum!
+    </p>
   </div>
 </template>
