ec175e36317bf04df219c37ae6817d179c52dae7
Elias Afara <afaraelias@gmail.com>
2022-09-16 12:12:44 +0200

    Created no tasks component

---
+++ vuetify-todo/src/components/Todo/NoTasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="no-tasks">
+    <v-icon size="100" color="primary  "> mdi-check </v-icon>
+    <div class="text-h5 primary--text">No tasks</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoTasks",
+};
+</script>
+
+<style lang="sass">
+.no-tasks
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    opacity: 0.5
+</style>
